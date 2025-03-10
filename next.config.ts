b58aa1de3f5d2daeefcdb3e64398898912b2c512
seagleax <litle.project@gmail.com>
2025-03-11 02:39:36 +0700

    deployment preparation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NEXT_PUBLIC_NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/reporistory-explorer/' : '',
+  basePath: isProd ? '/reporistory-explorer' : '',
+  output: 'export'
 };
 
 export default nextConfig;
